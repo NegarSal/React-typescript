4c3b3c303a0155a104d2c6907bc5edb5612b700d
Negar <negarsaljooghi@gmail.com>
2024-06-15 15:40:06 +0330

    Basic props: number type, boolean type

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name='mario' />
+      <Greet name='mario' messageCount={20} isLoggedIn={false } />
     </div>
   );
 }
