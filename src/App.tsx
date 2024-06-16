436822ba52b28bff762a6dc05e1467f46a033ab6
Negar <negarsaljooghi@gmail.com>
2024-06-16 14:52:15 +0330

    object type, array type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+  const nameList = [{
+    first: 'Bruce',
+    last: 'Wayne'
+  },
+  {
+    first: 'Clark',
+    last: 'Kent',
+  },
+  {
+    first: 'Peter',
+    last: 'Parker',
+  }]
+
   return (
     <div className="App">
-      <Greet name='mario' messageCount={20} isLoggedIn={false } />
+      <Greet name='mario' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
