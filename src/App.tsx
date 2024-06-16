c4b3c4dd78c9c6f775821b816bfa8eef46980d91
Negar <negarsaljooghi@gmail.com>
2024-06-16 15:40:49 +0330

    children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Status } from './components/Status';
 
 function App() {
   return (
     <div className="App">
       <Status status='loading' />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo DiCaprio</Heading>
+      </Oscar>
     </div>
   );
 }
