ec0ff5d1460974cf1272a7eb7e90e0d3adf56cef
Negar <negarsaljooghi@gmail.com>
2024-06-16 15:16:56 +0330

    union type

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-  const nameList = [{
-    first: 'Bruce',
-    last: 'Wayne'
-  },
-  {
-    first: 'Clark',
-    last: 'Kent',
-  },
-  {
-    first: 'Peter',
-    last: 'Parker',
-  }]
-
   return (
     <div className="App">
-      <Greet name='mario' messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <Status status='loading' />
     </div>
   );
 }
